6fa6c9d026d84adbc9ba5efed27c25a1a234d56a
Davor Kantor <davor1012@gmail.com>
2022-05-30 18:49:33 +0200

    landing page finished, mobile version and hamburger menu

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ const popImg = document.querySelector('.gallery_popup-img img');
 gallery.forEach(image => {
 
     popup.style.visibility = 'hidden';
-
     image.onclick = () => {
         popup.style.display = 'block';
         popup.style.visibility = 'visible';
@@ -18,7 +17,22 @@ gallery.forEach(image => {
     }
 });
 
-
 document.querySelector('.gallery_popup-img span').onclick = () => {
     popup.style.display = 'none';
 }
+
+
+// Hamburger menu
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.onclick = () =>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+document.querySelectorAll(".nav-link").forEach(n => n.onclick = () =>{
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+})
